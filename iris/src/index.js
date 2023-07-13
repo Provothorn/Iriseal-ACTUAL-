20e3fc30f62b4bca043b2442c8903463b4a74969
Swayamshree Nanda <swayamshree2001@gmail.com>
2023-07-14 00:24:25 +0530

    iris commit

---
+++ iris/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './home.css';
+import './login.css';
+
+
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
+import Loginpage from './pages/login';
+import Signuppage from './pages/signup';
+import Homepage from './pages/home';
+
+
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      <Route path="/" element={<Loginpage />} />
+      <Route path="/signuppage" element={<Signuppage />} />
+      <Route path="/homepage" element={<Homepage />} />
+      
+      </Routes>
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
